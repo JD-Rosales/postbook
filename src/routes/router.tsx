f130d37053d27c497220a63b4f64ad7763fb7bec
Jake <rosales.jake1999@gmail.com>
2023-05-01 21:52:02 +0800

    Fixed production rewrite rule

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -4,8 +4,7 @@ import { createBrowserRouter } from "react-router-dom";
 const BaseLayout = React.lazy(() => import("../layout/BaseLayout"));
 const Sidebar = React.lazy(() => import("../layout/Sidebar"));
 const ErrorPage = React.lazy(() => import("@pages/Error"));
-// const Signup = React.lazy(() => import("@pages/Signup"));
-import Signup from "@pages/Signup";
+const Signup = React.lazy(() => import("@pages/Signup"));
 const Login = React.lazy(() => import("@pages/Login"));
 
 export const router = createBrowserRouter([
