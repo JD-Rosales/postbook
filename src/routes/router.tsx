7f1f78f45f537dc763680b74175765712a42363a
Jake <rosales.jake1999@gmail.com>
2023-05-01 21:25:08 +0800

    Test build for 404 page on production

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -4,7 +4,8 @@ import { createBrowserRouter } from "react-router-dom";
 const BaseLayout = React.lazy(() => import("../layout/BaseLayout"));
 const Sidebar = React.lazy(() => import("../layout/Sidebar"));
 const ErrorPage = React.lazy(() => import("@pages/Error"));
-const Signup = React.lazy(() => import("@pages/Signup"));
+// const Signup = React.lazy(() => import("@pages/Signup"));
+import Signup from "@pages/Signup";
 const Login = React.lazy(() => import("@pages/Login"));
 
 export const router = createBrowserRouter([
